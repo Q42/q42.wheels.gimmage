3dadd8b1722460d95d607293d820fb22527fe3d5
Mark van Straten <mark@q42.nl>
2013-01-03 12:48:07 +0100

    added nuspec template, started getting it into shape

--- Q42.Wheels.Gimmage.Mvc/GimmageServer.cs
+++ Q42.Wheels.Gimmage.Mvc/GimmageServer.cs
@@ -3,6 +3,9 @@ using Q42.Wheels.Gimmage.Interfaces;
 
 namespace Q42.Wheels.Gimmage
 {
+  /// <summary>
+  /// This class is the MVC implementation singleton for MVC
+  /// </summary>
   public static class GimmageServer
   {
     private static readonly IHttpImageServer imageServer = new HttpImageServer();
@@ -15,6 +18,10 @@ namespace Q42.Wheels.Gimmage
       }
     }
 
+    /// <summary>
+    /// Register the default gimmage routes in your applications MVC route table
+    /// </summary>
+    /// <param name="routes"></param>
     public static void RegisterRoutes(RouteCollection routes)
     {
       routes.Add(new GimmageRoute());
